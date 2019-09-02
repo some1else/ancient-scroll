f709da3067617a7a01e4a42236b5a04f9c35d274
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-09-03 00:02:42 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { PureComponent, Fragment } from "react"
+import PropTypes from "prop-types"
 
-import styles from './styles.css'
+// import { ReactComponent as ScrollSVG } from "./assets/ancient-scroll.svg"
+// const scrollImage = "https://i.imgur.com/CqkYoPm.png"
+// const opacityMask = "https://i.imgur.com/4AUs6ML.png"
 
-export default class ExampleComponent extends Component {
+import styles from "./styles.css"
+
+// const scrollStyle = {
+//   backgroundImage: `url(${scrollImage})`,
+//   maskImage: `url(${opacityMask})`,
+// }
+
+export default class ExampleComponent extends PureComponent {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const { children } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <Fragment>
+        <div className={styles.AncientScroll}>
+          <div className={styles.skew}>{children}</div>
+        </div>
+        <div className={styles.AncientScrollBkg} />
+      </Fragment>
     )
   }
 }
