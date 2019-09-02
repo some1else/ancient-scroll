7983f964a6c1161697d132c93928a775bf013451
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-09-03 00:02:29 +0200

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,81 @@
-import React, { Component } from 'react'
+import React, { PureComponent, Fragment } from "react"
 
-import ExampleComponent from 'ancient-scroll'
+import AncientScroll from "ancient-scroll"
 
-export default class App extends Component {
-  render () {
+export default class App extends PureComponent {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <Fragment>
+        <div className="layout">
+          <h1>Ancient Scroll JS</h1>
+          <p>
+            Enables automatic old-style scrolling of contents in React
+            JavaScript libery.
+          </p>
+        </div>
+        <AncientScroll>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+          <p>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. In maximus, augue a facilisis tempor, sem
+            erat posuere arcu, eu dictum lorem lacus ac nulla. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Maecenas mollis velit vel
+            consequat elementum. Fusce ut augue vel velit varius egestas id in
+            neque. Nulla consequat lacus in ipsum tempor iaculis. Donec quis
+            odio eu neque volutpat sodales. Mauris sed laoreet elit. Sed eu
+            libero sem.
+          </p>
+        </AncientScroll>
+      </Fragment>
     )
   }
 }
